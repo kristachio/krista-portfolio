1930adc40c825caafdf7039c08c364a5b218fca6
Krista Frietas <krista.frietas@gmail.com>
2023-04-14 12:11:27 -0400

    Header links and burger bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,49 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from '../app/assets/img/logo.png';
+import { useState } from 'react';
+import { 
+    Navbar, 
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav, 
+    NavItem,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import Logo from '../app/assets/img/logo.png';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <Navbar dark color='primary' sticky='top' expand='md'>
-            <NavbarBrand href='/'>
-              <img src={NucampLogo} alt='nucamp logo' />
+            <NavbarBrand className='ms-5' href='/'>
+                <img src={Logo} alt='logo' className='float-start' />
+                <h1 className='mt-1'></h1>
             </NavbarBrand>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            <i className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/projects'>
+                            <i className='fa fa-list fa-lg' /> Projects
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/about'>
+                            <i className='fa fa-info fa-lg' /> About
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/contact'>
+                            <i className='fa fa-address-card fa-lg' /> Contact
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
         </Navbar>
     );
 };
